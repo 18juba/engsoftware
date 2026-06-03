cb6ae159c914375f31f217cb057dcac2f0a124ab
18juba <jubalucas05@gmail.com>
2026-06-03 17:33:52 -0300

    server

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello, World!")
+	server := gin.Default()
+
+	server.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	server.Run(":8000")
 }
