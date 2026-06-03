fcfa467c67a6e3ee523842141d07c0398cf6e879
18juba <jubalucas05@gmail.com>
2026-06-03 18:01:23 -0300

    servindo corretamente

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,5 +17,29 @@ func main() {
 
 	setupRoutes(server)
 
-	server.Run(":8000")
+	srv := &http.Server{
+		Addr:    "0.0.0.0:8000",
+		Handler: server,
+	}
+
+	go func() {
+		log.Printf("Iniciando servidor em %s", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Erro ao iniciar servidor: %v", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	log.Println("Encerrando servidor...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("Erro ao encerrar servidor: %v", err)
+	}
+
+	log.Println("Servidor encerrado")
 }
